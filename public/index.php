e6e28a53413baf4596c8a70e9c1e66aef3c9a42a
Faventyne <guitchat@gmail.com>
2017-11-06 00:07:41 +0100

    Push sunday night

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
+ini_set('display_errors',1);
+error_reporting(E_ALL | E_STRICT);
 require_once __DIR__ .'/../inc/config.php';
-
-
 require_once __DIR__ .'/../view/header.php';
 require_once __DIR__ .'/../view/home.php';
 require_once __DIR__ .'/../view/footer.php';
+
+
 
  ?>
